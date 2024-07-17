0e67d652132c5d6f41d938027fe6c20815037903
Shivang Rajouria <shivangrajouria17@gmail.com>
2024-07-18 02:44:40 +0530

    feat: implemented upload file logic

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 
 const app=express();
-
+app.use(cors());
 const PORT = 5000;
+
+if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
+    fs.mkdirSync(path.join(__dirname, 'uploads'));
+}
+
+if (!fs.existsSync(path.join(__dirname, 'videos'))) {
+    fs.mkdirSync(path.join(__dirname, 'videos'));
+}
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('video'), (req, res) => {
+    const filePath = path.join(__dirname, req.file.path);
+    console.log(`File uploaded to: ${filePath}`);
+    res.send({ path: filePath });
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
